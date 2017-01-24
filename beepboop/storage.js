0957dac0dfb79137f1f0fad344e38301702a62d2
Brad Harris <bmharris@gmail.com>
2017-01-24 16:47:21 -0700

    refactoring to support beep boop

--- beepboop/storage.js
+++ beepboop/storage.js
@@ -1,12 +1,12 @@
+const deap = require('deap')
 const Persist = require('beepboop-persist')
 const Teams = require('./teams')
 
-module.exports = function(config) {
-
+module.exports = function (config) {
   var persist = Persist()
   var bbTeams = Teams({
     token: process.env.BEEPBOOP_TOKEN,
-    url: process.env.BEEPBOOP_PERSIST_URL,
+    url: process.env.BEEPBOOP_PERSIST_URL
   })
 
   var storage = {
@@ -16,7 +16,7 @@ module.exports = function(config) {
           return cb(new Error('Missing teamId in storage.teams.get()'))
         }
 
-        // load slack team data from teams api and overlay it onto any data stored in persist 
+        // load slack team data from teams api and overlay it onto any data stored in persist
         bbTeams.get(teamId, (err, team) => {
           if (err) {
             return cb(err)
@@ -26,7 +26,7 @@ module.exports = function(config) {
             return cb(new Error(`No team found for team id ${teamId}`))
           }
 
-          persist.get(`teams/${teamId}`, (err, teamData) => {
+          persist.get(teamKey(teamId), (err, teamData) => {
             if (err) {
               return cb(err)
             }
@@ -42,17 +42,11 @@ module.exports = function(config) {
           return cb(new Error('Missing team.id in storage.teams.save()'))
         }
 
-        persist.set(`teams/${team.id}`, team, cb)
+        persist.set(teamKey(team.id), team, cb)
       },
-      // Deletes team data in persist along w/ team record in Beep Boop
+      // Deletes team data in persist
       delete (teamId, cb) {
-        bbTeams.del(teamId, err => {
-          if (err) {
-            return cb(err)
-          }
-
-          persist.del(`teams/${teamId}`, cb)
-        })
+        persist.del(teamKey(teamId), cb)
       },
       all (cb) {
         // TODO: currently this will only grab the first 1000 teams - need to make paginated calls
@@ -62,7 +56,7 @@ module.exports = function(config) {
             return cb(err)
           }
 
-          let teamKeys = teams.map(team => `teams/${team.slack_team_id}`)
+          let teamKeys = teams.map(team => teamKey(team.slack_team_id))
 
           // Next get any metadata saved for each team
           persist.mget(teamKeys, (err, teamsData) => {
@@ -72,7 +66,7 @@ module.exports = function(config) {
 
             // merge team metadata w/ beep boop teams and transform to botkit team
             // indexes between two should be the same
-            let botkitTeams = teamData.map((teamData, idx) => {
+            let botkitTeams = teamsData.map((teamData, idx) => {
               return deap(teamData || {}, bbTeamToBotkitTeam(teams[idx]))
             })
 
@@ -83,13 +77,13 @@ module.exports = function(config) {
     },
     users: {
       get (userId, cb) {
-        persist.get(`users/${userId}`, cb)
+        persist.get(userKey(userId), cb)
       },
       save (user, cb) {
-        persist.set(`users/${user.id}`, user, cb)
+        persist.set(userKey(user.id), user, cb)
       },
       delete (userId, cb) {
-        persist.del(`users/${userId}`, cb)
+        persist.del(userKey(userId), cb)
       },
       all (cb) {
         // get all the user keys
@@ -104,13 +98,13 @@ module.exports = function(config) {
     },
     channels: {
       get (channelId, cb) {
-        persist.get(`channels/${channelId}`, cb)
+        persist.get(channelKey(channelId), cb)
       },
       save (channel, cb) {
-        persist.set(`channels/${channel.id}`, channel, cb)
+        persist.set(channelKey(channel.id), channel, cb)
       },
       delete (channelId, cb) {
-        persist.del(`channels/${channelId}`, cb)
+        persist.del(channelKey(channelId), cb)
       },
       all (cb) {
         // get all the channel keys
@@ -125,7 +119,19 @@ module.exports = function(config) {
     }
   }
 
-  return storage;
+  return storage
+}
+
+function teamKey (id) {
+  return `teams:${id}`
+}
+
+function userKey (id) {
+  return `users:${id}`
+}
+
+function channelKey (id) {
+  return `channels:${id}`
 }
 
 // transform beep boop team into what botkit expects
@@ -136,6 +142,7 @@ function bbTeamToBotkitTeam (team) {
     url: `https://${team.slack_team_domain}.slack.com/`,
     name: team.slack_team_name,
     bot: {
+      name: team.slack_bot_user_name,
       token: team.slack_bot_access_token,
       user_id: team.slack_bot_user_id,
       createdBy: team.slack_user_id,
---
+++ components/webserver/verify-token.js
@@ -0,0 +1,20 @@
+module.exports = token => {
+  return function verifyTokenMiddleware (req, res, next) {
+    // If token isn't set, we're not verifying
+    if (!token) {
+      return next()
+    }
+
+    let verifyToken = (req.body || {}).token
+
+    // test verify token
+    console.log('token: %s - verifyToken: %s', token, verifyToken)
+    if (token !== verifyToken) {
+      console.log('Invalid verify token')
+      res.status(403).send('Invalid verify token')
+      return
+    }
+
+    next()
+  }
+}
